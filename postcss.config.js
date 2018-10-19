a07dfe07c6ebaf736d4938bcaeb21622a29ba5c2
Dusan Mijatovic (Zenbook 2018) <d.mijatovic@dv4all.nl>
2018-10-19 21:06:22 +0200

    IE11 grid fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,12 @@
 module.exports = () => {
-  //debugger
+  console.log("postcss.config.js...running")
   return {
     plugins:[
       require('postcss-preset-env')({
         autoprefixer:{
           grid: true,
-          flexbox: true,
-        },
-        stage:3
+          flexbox: true
+        }
       })
     ]
   }
